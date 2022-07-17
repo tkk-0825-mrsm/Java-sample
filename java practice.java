5ff7df5d3cb4b37c5568726c0b4249cc66120253
Morishima Takako <tkkhr.0322@outlook.jp>
2022-07-17 20:51:17 +0900

    エラー発生

---
+++ java practice.java
@@ -0,0 +1,64 @@
+int luckyNumber;
+luckyNumber = 7;
+int luckyNumber = 7;
+String name; name = "名前";
+char color = '赤';
+String name = "名前";
+finak int LUCKEY_NUMBER = 7;
+LUCKEY_NUMBER = 777;
+//コンパイルエラーとなる。
+System.out.println("こんにちは\nfoo barです。");
+String[] weekdays;
+weekdays = new String[7];
+weekdays[0] ="日";
+weekdays[1] ="月";
+weekdays[2] ="火";
+weekdays[3] ="水";
+weekdays[4] ="木";
+weekdays[5] ="金";
+weekdays[6] ="土";
+String[] weekdays = new String[] {"日","月","火","水",
+"木","金","土"};
+String[] weekdays = {"日","月","火","水","木","金",
+"土"};
+//コメントの一文です
+int number;
+/*
+コメントの1行目です
+コメントの2行目です
+コメントの3行目です
+*/
+/**
+* クラスの説明.
+* @author foo bar
+* @version 1.8
+* @since 1.0
+*/
+public class HelloWorld {
+
+    /**メッセージ*/
+    private static final String MESSAGE = "Hello World!";
+
+/**
+ * メゾットの説明.
+ * メゾットの詳細を記述する.
+ * @param args コマンドライン引数
+ */
+ public satic void main(String[] args) {
+    System.out.println(MESSAGE);
+   }
+
+ }
+
+ int i = 1;
+ if (i == 1){
+    //条件が一致した場合の処理を記述
+ }
+ int i = 1;
+ if (i == 1) {
+    //条件が一致した場合の処理を記述
+ }
+ int i = 1;
+ if (i != 1) {
+ }
+
